b6790dc139c2b4fe0c8a106e2851ee7a21da36a4
Luiz Felipe Santana Dos Santos <luiz.santos811@etec.sp.gov.br>
2022-09-28 19:08:17 -0300

    update 22/09

---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatTableModule} from '@angular/material/table';
+
+import { CursosRoutingModule } from './cursos-routing.module';
+import { ListacursosComponent } from './listacursos/listacursos.component';
+import {MatCardModule} from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    ListacursosComponent
+  ],
+  imports: [
+    CommonModule,
+    CursosRoutingModule,
+    MatTableModule,
+    MatCardModule
+  ]
+})
+export class CursosModule { }
---
+++ src/app/cursos/listacursos/listacursos.component.ts
@@ -0,0 +1,23 @@
+
+import { Curso } from './../../core/model';
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from '../cursos.service';
+
+@Component({
+  selector: 'app-listacursos',
+  templateUrl: './listacursos.component.html',
+  styleUrls: ['./listacursos.component.scss']
+})
+export class ListacursosComponent implements OnInit {
+
+  cursos: Curso[]= [];
+
+  displayedColumns =['id','nomecurso']
+
+  constructor(private cursosService: CursosService) { }
+
+  ngOnInit(): void {
+    this.cursos = this.cursosService.listarCursos()
+  }
+
+}
